4a8adf17b3e6d2eab53761c9d923381208b220a5
Bekforever13 <b3kforever@gmail.com>
2023-11-13 10:22:54 +0500

    fixed login page

--- src/components/screens/Login/Login.tsx
+++ src/components/screens/Login/Login.tsx
@@ -7,14 +7,19 @@ import { ILoginDataBody } from 'src/store/auth/Auth.types'
 import { formatPhone } from 'src/utils/shared'
 import { message } from 'antd'
 import { MaskedInput } from 'antd-mask-input'
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
 
 const Login: React.FC = () => {
+	const [isButtonDisabled, setButtonDisabled] = React.useState(false)
+	const [showPassword, setShowPassword] = React.useState(false)
 	const {
 		register,
 		handleSubmit,
 		control,
-		formState: { errors },
-	} = useForm<ILoginDataBody>()
+		formState: { isValid },
+	} = useForm<ILoginDataBody>({
+		mode: 'onChange',
+	})
 	const navigate = useNavigate()
 	const token = localStorage.getItem('token')
 	const { setAuth } = useActions()
@@ -24,6 +29,7 @@ const Login: React.FC = () => {
 	)
 
 	const onSubmit = async (values: ILoginDataBody) => {
+		setButtonDisabled(true)
 		await login({ ...values, phone: formatPhone(values.phone) })
 	}
 
@@ -33,7 +39,9 @@ const Login: React.FC = () => {
 			setAuth(true)
 			navigate('/')
 		}
-		if (isError) message.error('Произошла ошибка. Повторите попытку!')
+		if (isError) {
+			message.error('Телефон или пароль введён неправильно. Повторите попытку')
+		}
 	}, [isSuccess, isError])
 
 	React.useEffect(() => {
@@ -43,15 +51,32 @@ const Login: React.FC = () => {
 		}
 	}, [token, checkedUser])
 
+	React.useEffect(() => {
+		let timer: ReturnType<typeof setTimeout>
+		if (isButtonDisabled) {
+			timer = setTimeout(() => {
+				setButtonDisabled(false)
+			}, 3000)
+		}
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [isButtonDisabled])
+
 	return (
 		<form
 			onSubmit={handleSubmit(onSubmit)}
-			className='flex items-center justify-center h-screen bg-[#F5F2DF] flex-col gap-y-5'
+			className='flex items-center justify-center h-screen bg-[#F5F2DF] flex-col gap-y-5 select-none'
 		>
-			<h1 className='font-bold text-xl'>Authorization</h1>
+			<h1 className='font-bold text-xl'>Вход в аккаунт</h1>
 			<Controller
 				name='phone'
 				control={control}
+				rules={{
+					required: true,
+					validate: value =>
+						value.replace(/\D/g, '').length > 3 || 'Введите телефон',
+				}}
 				render={({ field }) => (
 					<MaskedInput
 						{...field}
@@ -61,19 +86,24 @@ const Login: React.FC = () => {
 					/>
 				)}
 			/>
-
-			{errors.phone && <div>Phone is required</div>}
-			<input
-				className='w-[300px] px-4 py-2 rounded-md border outline-none'
-				type='password'
-				{...register('password', { required: true })}
-				placeholder='Пароль'
-			/>
-			{errors.password && <div>Password is required</div>}
+			<div className='relative'>
+				<input
+					className='w-[300px] px-4 py-2 rounded-md border outline-none'
+					type={showPassword ? 'text' : 'password'}
+					{...register('password', { required: true })}
+					placeholder='Пароль'
+				/>
+				<div
+					className='absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer'
+					onClick={() => setShowPassword(!showPassword)}
+				>
+					{showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+				</div>
+			</div>
 			<button
 				className='w-[300px] p-3 border rounded-md bg-[#F4C95B] text-white font-bold '
 				type='submit'
-				disabled={isLoading}
+				disabled={isLoading || !isValid || isButtonDisabled}
 			>
 				{isLoading ? 'Загрузка...' : 'Войти'}
 			</button>
