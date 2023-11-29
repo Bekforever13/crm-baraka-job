a8b55abab300684339ce2e3dcaae29b3bfc7e3a1
Bekforever13 <b3kforever@gmail.com>
2023-11-29 17:02:38 +0500

    fixed

--- src/components/screens/Login/Login.tsx
+++ src/components/screens/Login/Login.tsx
@@ -102,6 +102,7 @@ const Login: React.FC = () => {
 						{...field}
 						mask='+\9\9\899 999 99 99'
 						className='w-[300px] px-4 py-2 rounded-md border outline-none'
+						placeholder='Телефон'
 					/>
 				)}
 			/>
